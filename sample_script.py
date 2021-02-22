9c6abdf065da62303458d724ea6de05189c3594b
Martin Božič <mb0484@student.uni-lj.si>
2021-02-22 17:30:36 +0100

    Update sample_script.py
    Modified sample_script

--- sample_script.py
+++ sample_script.py
@@ -5,10 +5,6 @@ import bcrypt
 #salt = bcrypt.gensalt()
 #hashed = b'$2b$12$iwxCSPfd/n9NAY5iW3/FDObDhyWRsAVxSkYUc2tRuH2Z/KoCf7hv.' #bcrypt.hashpw(passwd, salt)
 
-passwd_objava = b'kreiraj_objavo_vincencij'
-passwd_edit_objava = b'uredi_objavo_vincencij'
-passwd_delete_objava = b'izbrisi_objavo_vincencij'
-
 #salt = bcrypt.gensalt()
 #hashed = bcrypt.hashpw(passwd_delete_objava, salt)
 hashed = b'$2b$12$LmoDxi1PxmQYGZDGDXlCgu7SQb72UFnAgaskoIG2eXCzElpRnIsby'
